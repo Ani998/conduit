264ad5e439a914a6c81232837564f5dc671baf13
Ani998 <trinyik.aniko@gmail.com>
2023-04-11 20:30:50 +0200

    +Registration

--- viszgaremek_teszt/conduit_test.py
+++ viszgaremek_teszt/conduit_test.py
@@ -26,20 +26,37 @@ class TestConduit(object):
     def teardown_method(self):
         self.browser.quit()
 
-#ATC_001 - ADATKEZELÉSI NYILATKOZAT HASZNÁLATA (Sütik elfogadása)
+#ATC001 - ADATKEZELÉSI NYILATKOZAT HASZNÁLATA (Sütik elfogadása)
     def test_accept_cookies(self):
         accept_btn = self.browser.find_element(By.XPATH, "//button[@class= 'cookie__bar__buttons__button cookie__bar__buttons__button--accept']")
         accept_btn.click()
-        cookie_accept = self.browser.get_cookie("vue-cookie-accept-decline-cookie-policy-panel")
-
-        assert cookie_accept["value"] == "accept"
         time.sleep(2)
 
-# ATC_002 - REGISZTRÁCIÓ
+        cookie_accepted = self.browser.get_cookie("vue-cookie-accept-decline-cookie-policy-panel")
+        assert cookie_accepted["value"] == "accept"
+        time.sleep(2)
+
+# ATC002 - REGISZTRÁCIÓ
+    def test_registration(self):
+        sign_up_btn = self.browser.find_element(By.LINK_TEXT, 'Sign up')
+        sign_up_btn.click()
+
+        reg_username = self.browser.find_element(By.XPATH, '//input[@placeholder="Username"]')
+        reg_username.send_keys("Valami2")
+        reg_email = self.browser.find_element(By.XPATH, '//input[@placeholder="Email"]')
+        reg_email.send_keys("valami2@gmail.com")
+        reg_psw = self.browser.find_element(By.XPATH, '//input[@placeholder="Password"]')
+        reg_psw.send_keys("Strukturavaltas3")
+        time.sleep(2)
+        reg_btn = self.browser.find_element(By.XPATH, '//button[@class="btn btn-lg btn-primary pull-xs-right"]')
+        reg_btn.click()
+
+        registration_message = WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.XPATH, '//div[@class="swal-title"]')))
+        assert registration_message.text == "Welcome!"
 
 
+# ATC003 - BEJELENTKEZÉS
 
-# ATC_003 - BEJELENTKEZÉS
 
 # ATC_004 - ADATOK LISTÁZÁSA
 # ATC_005 - TÖBB OLDALAS LISTA BEJÁRÁSA
